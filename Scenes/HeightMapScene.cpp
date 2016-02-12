2dbd75dc2fcf5cb2c345f8c0c076622131dd3121
Mathias Soeholm <mathiassoeholm@gmail.com>
2016-02-12 23:46:47 +0100

    Started working on Height Map scene

---
+++ Scenes/HeightMapScene.cpp
@@ -0,0 +1,11 @@
+#include "HeightMapScene.h"
+
+void HeightMapScene::init(GLFWwindow *window, int screenWidth, int screenHeight)
+{
+	camera = Camera(window, Vector3f(), Quaternionf());
+}
+
+void HeightMapScene::run(GLFWwindow *window)
+{
+
+}
--- Scenes/HeightMapScene.h
+++ Scenes/HeightMapScene.h
@@ -4,25 +4,12 @@
 #include <GLFW/glfw3.h>
 #include "../Camera.h"
 
-class WorldScene : public IScene
+class HeightMapScene : public IScene
 {
-	GLuint _vao;
-	GLuint _shaderProgram;
-	GLuint _viewMatLocation;
-	GLuint _modelMatLocation;
-	GLuint _colorLocation;
-	Matrix4x4f _modelMat;
-	Camera * _camera;
-	Vector4f * _triangleColor;
+	Camera camera;
 public:
-	~WorldScene();
-	WorldScene() : _camera{ nullptr }, _triangleColor{ nullptr }, _modelMat(Matrix4x4f())
-	{
-
-	};
 	void init(GLFWwindow* window, int screenWidth, int screenHeight) override;
 	void run(GLFWwindow* window) override;
 	void onWindowSizeChanged(int width, int height) override;
 	void onKeyEvent(GLFWwindow* window, int key, int scancode, int action, int mods) override;
 };
-
