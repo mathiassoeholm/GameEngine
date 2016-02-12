671f7be175c8207606e0f80c0dc86842bd5324f6
Mathias Soeholm <mathiassoeholm@gmail.com>
2016-02-12 23:56:57 +0100

    GlFloat array

--- Scenes/HeightMapScene.cpp
+++ Scenes/HeightMapScene.cpp
@@ -2,7 +2,15 @@
 
 void HeightMapScene::init(GLFWwindow *window, int screenWidth, int screenHeight)
 {
+	const int SEGMENTS = 100;
+
 	camera = Camera(window, Vector3f(), Quaternionf());
+	GLfloat vertices[SEGMENTS * SEGMENTS * 5];
+
+
+
+	// Load image
+	// Generate vertices
 }
 
 void HeightMapScene::run(GLFWwindow *window)
