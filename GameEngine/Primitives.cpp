35bdcc0404bbbcdad5cb0a5357d66d542fac3d69
Mathias Soeholm <mathiassoeholm@gmail.com>
2016-08-09 01:41:20 +0200

    Cube render function now works

--- GameEngine/Primitives.cpp
+++ GameEngine/Primitives.cpp
@@ -98,107 +98,83 @@ namespace GameEngine
 
 		Vertex vertices[] =
 		{
-				// Front
-				glm::vec3(-0.5f, 0.5f, 0.0f), // Position
-				glm::vec3(0.0f, 0.0f, 1.0f), // Normal
-				glm::vec3(1.0f, 1.0f, 1.0f), // Color
+				glm::vec3(-1.0f, +1.0f, +1.0f), // 0
+				glm::vec3(+1.0f, +0.0f, +0.0f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(+1.0f, +1.0f, +1.0f), // 1
+				glm::vec3(+0.0f, +1.0f, +0.0f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(+1.0f, +1.0f, -1.0f), // 2
+				glm::vec3(+0.0f, +0.0f, +1.0f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(-1.0f, +1.0f, -1.0f), // 3
+				glm::vec3(+1.0f, +1.0f, +1.0f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
 
-				glm::vec3(0.5f, 0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
+				glm::vec3(-1.0f, +1.0f, -1.0f), // 4
+				glm::vec3(+1.0f, +0.0f, +1.0f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(+1.0f, +1.0f, -1.0f), // 5
+				glm::vec3(+0.0f, +0.5f, +0.2f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(+1.0f, -1.0f, -1.0f), // 6
+				glm::vec3(+0.8f, +0.6f, +0.4f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(-1.0f, -1.0f, -1.0f), // 7
+				glm::vec3(+0.3f, +1.0f, +0.5f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
 
-				glm::vec3(0.5f, -0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
+				glm::vec3(+1.0f, +1.0f, -1.0f), // 8
+				glm::vec3(+0.2f, +0.5f, +0.2f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(+1.0f, +1.0f, +1.0f), // 9
+				glm::vec3(+0.9f, +0.3f, +0.7f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(+1.0f, -1.0f, +1.0f), // 10
+				glm::vec3(+0.3f, +0.7f, +0.5f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(+1.0f, -1.0f, -1.0f), // 11
+				glm::vec3(+0.5f, +0.7f, +0.5f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
 
-				glm::vec3(-0.5f, -0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
+				glm::vec3(-1.0f, +1.0f, +1.0f), // 12
+				glm::vec3(+0.7f, +0.8f, +0.2f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(-1.0f, +1.0f, -1.0f), // 13
+				glm::vec3(+0.5f, +0.7f, +0.3f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(-1.0f, -1.0f, -1.0f), // 14
+				glm::vec3(+0.4f, +0.7f, +0.7f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(-1.0f, -1.0f, +1.0f), // 15
+				glm::vec3(+0.2f, +0.5f, +1.0f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
 
-				// Back
-				glm::vec3(-0.5f, 0.5f, 1.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
+				glm::vec3(+1.0f, +1.0f, +1.0f), // 16
+				glm::vec3(+0.6f, +1.0f, +0.7f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(-1.0f, +1.0f, +1.0f), // 17
+				glm::vec3(+0.6f, +0.4f, +0.8f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(-1.0f, -1.0f, +1.0f), // 18
+				glm::vec3(+0.2f, +0.8f, +0.7f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(+1.0f, -1.0f, +1.0f), // 19
+				glm::vec3(+0.2f, +0.7f, +1.0f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
 
-				glm::vec3(0.5f, 0.5f, 1.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(0.5f, -0.5f, 1.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(-0.5f, -0.5f, 1.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				// Bottom
-				glm::vec3(-0.5f, -0.5f, 1.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(0.5f, -0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(0.5f, -0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(-0.5f, -0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				// Top
-				glm::vec3(-0.5f, 0.5f, 1.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(0.5f, 0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(0.5f, 0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(-0.5f, 0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				// Left
-				glm::vec3(-0.5f, 0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(-0.5f, 0.5f, -1.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(-0.5f, -0.5f, -1.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(-0.5f, -0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				// Right
-				glm::vec3(0.5f, 0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(0.5f, 0.5f, -1.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(0.5f, -0.5f, -1.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f),
-
-				glm::vec3(0.5f, -0.5f, 0.0f),
-				glm::vec3(0.0f, 0.0f, 1.0f),
-				glm::vec3(1.0f, 1.0f, 1.0f)
+				glm::vec3(+1.0f, -1.0f, -1.0f), // 20
+				glm::vec3(+0.8f, +0.3f, +0.7f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(-1.0f, -1.0f, -1.0f), // 21
+				glm::vec3(+0.8f, +0.9f, +0.5f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(-1.0f, -1.0f, +1.0f), // 22
+				glm::vec3(+0.5f, +0.8f, +0.5f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
+				glm::vec3(+1.0f, -1.0f, +1.0f), // 23
+				glm::vec3(+0.9f, +1.0f, +0.2f), // Color
+				glm::vec3(0, 0, 2.0f), // Normal
 		};
 
 		cube.vertices = new Vertex[cube.numVertices];
@@ -208,12 +184,12 @@ namespace GameEngine
 
 		GLushort indices[] =
 		{
-			0, 1, 3,		1, 2, 3, // Front
-			4, 5, 6,		6, 7, 4, // Back
-			8, 9, 10,		10, 11, 8, // Bottom
-			12, 13, 14,		14, 15, 12, // Top
-			16, 17, 18,		18, 19, 16, // Left
-			20, 21, 22,		22, 23, 20 // Right
+				0,   1,  2,  0,  2,  3, // Top
+				4,   5,  6,  4,  6,  7, // Front
+				8,   9, 10,  8, 10, 11, // Right
+				12, 13, 14, 12, 14, 15, // Left
+				16, 17, 18, 16, 18, 19, // Back
+				20, 22, 21, 20, 23, 22, // Bottom
 		};
 
 		cube.indices = new GLushort[cube.numIndices];
